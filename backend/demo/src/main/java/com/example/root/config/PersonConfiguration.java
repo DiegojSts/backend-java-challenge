c0fb4626a3a2b791f7dd8834ee46ccc3b58ed918
Diego Santos <diegojs03@hotmail.com>
2023-01-12 01:54:23 -0300

    Configured Person Configuration for testing input data

---
+++ backend/demo/src/main/java/com/example/root/config/PersonConfiguration.java
@@ -0,0 +1,48 @@
+package com.example.root.config;
+
+import com.example.root.model.Adress;
+import com.example.root.model.Person;
+import com.example.root.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class PersonConfiguration {
+
+    @Bean
+    CommandLineRunner commandLineRunner (PersonRepository personRepository){
+        return args -> {
+            Adress adress1 = new Adress(
+                    "M.Navarro",
+                    "Ba",
+                    "000",
+                    "SSA"
+            );
+
+            Adress adress2 = new Adress(
+                    "Amazonas",
+                    "Ba",
+                    "333",
+                    "SSA"
+            );
+
+
+            Person person = new Person(
+                    "Diego",
+                    LocalDate.of(1997, Month.FEBRUARY, 1)
+            );
+
+            person.setAdress(List.of(adress1, adress2));
+            personRepository.saveAll(List.of(person));
+
+
+
+        };
+    }
+}
