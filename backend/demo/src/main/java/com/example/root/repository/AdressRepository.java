18f3910866c5b1b9b74175a069b29efe79251a44
Diego Santos <diegojs03@hotmail.com>
2023-01-12 01:55:30 -0300

    Created Adress Repository

---
+++ backend/demo/src/main/java/com/example/root/repository/AdressRepository.java
@@ -0,0 +1,7 @@
+package com.example.root.repository;
+
+import com.example.root.model.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdressRepository extends JpaRepository<Adress, Long> {
+}
