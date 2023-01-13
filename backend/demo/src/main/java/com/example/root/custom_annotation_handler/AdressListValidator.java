704eb1c37aecd4702a164d169cb06513aa960f55
Diego Santos <diegojs03@hotmail.com>
2023-01-13 00:09:12 -0300

    Created custom annotation for handling empty adress exception

---
+++ backend/demo/src/main/java/com/example/root/custom_annotation_handler/AdressListValidator.java
@@ -0,0 +1,17 @@
+package com.example.root.custom_annotation_handler;
+
+import org.springframework.util.CollectionUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.Objects;
+
+public class AdressListValidator implements ConstraintValidator <ValidAdressList, List<? extends Object>> {
+
+    @Override
+    public boolean isValid(List<? extends Object> value, ConstraintValidatorContext context) {
+        System.out.println(value);
+        return !(!CollectionUtils.isEmpty(value) || value == null || value.isEmpty() || value.stream().anyMatch(Objects::isNull));
+    }
+}
---
+++ backend/demo/src/main/java/com/example/root/custom_annotation_handler/ValidAdressList.java
@@ -0,0 +1,23 @@
+package com.example.root.custom_annotation_handler;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+        ElementType.FIELD,
+        ElementType.PARAMETER,
+        ElementType.ANNOTATION_TYPE
+})
+@Constraint(validatedBy = AdressListValidator.class)
+public @interface ValidAdressList {
+    String message() default "Deve existir ao menos 1 endereço e campos de endereço são obrigatórios!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
