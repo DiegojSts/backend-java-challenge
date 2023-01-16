8f5f0d697e1d1abae4c2c47c3874f4ac99dbe371
Diego Santos <diegojs03@hotmail.com>
2023-01-16 00:14:08 -0300

    Updated returning errors exceptions

--- backend/demo/src/main/java/com/example/root/services/PersonService.java
+++ backend/demo/src/main/java/com/example/root/services/PersonService.java
@@ -1,18 +1,13 @@
 package com.example.root.services;
-
-import com.example.root.exception.BadRequestException;
+import com.example.root.exception.NotFoundException;
 import com.example.root.model.Adress;
 import com.example.root.model.Person;
 import com.example.root.repository.AdressRepository;
 import com.example.root.repository.PersonRepository;
-
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class PersonService {
@@ -24,77 +19,37 @@ public class PersonService {
         this.adressRepository = adressRepository;
     }
 
-    public Person addPerson(Person person) {
+    public Person addPerson(Person person) throws IllegalArgumentException{
         List<Adress> adresses = person.getAdress();
 
         long countTrue = adresses.stream().filter(Adress::isMainAdress).count();
-        if (countTrue > 1) throw new BadRequestException("Deve existir apenas um endereço principal");
+        if (countTrue > 1) throw new IllegalArgumentException("Deve existir apenas um endereço principal");
 
         return personRepository.save(person);
+
     }
 
 
     public List<Person> getAllPerson() {
-        return personRepository.findAll();
+         return personRepository.findAll();
     }
 
-    public Person findPersonById(Long id) throws IllegalArgumentException {
+    public Person findPersonById(Long id) throws NotFoundException {
 
         Optional<Person> person = personRepository.findById(id);
         if(!person.isPresent())
-            throw new BadRequestException("Pessoa de ID " + id + " não foi encontrada.");
+            throw new NotFoundException("Pessoa de ID " + id + " não foi encontrada");
         return person.get();
     }
 
     public List<Adress> findPersonAdressById(Long id) {
         Optional<Person> person = Optional.ofNullable(personRepository.findById(id)
-                .orElseThrow(() -> new IllegalStateException("Endereço de ID" + id + "não foi encontrado.")));
-
-//        if(!person.isPresent())
-//            throw new IllegalArgumentException("Endereço de ID " + id + " não foi encontrada.");
+                .orElseThrow(() -> new NotFoundException("Endereço de ID" + id + "não foi encontrado.")));
 
         return person.get().getAdress();
     }
 
-//    public Person updatePerson(Long id, Person person) {
-//        Optional<Person> optionalPerson = Optional.ofNullable(personRepository.findById(id)
-//                .orElseThrow(() -> new IllegalStateException("Pessoa de ID" + id + "não foi encontrada.")));
-//
-////        if(!optionalPerson.isPresent())
-////            throw new IllegalArgumentException("Pessoa de ID " + id + " não foi encontrada.");
-//
-////        Person existingPerson = new Person(person, id);
-//
-//        var listIds = optionalPerson.get().getAdress().stream().map(Adress::getId).collect(Collectors.toList());
-//
-////        Person existingPerson = optionalPerson.get();
-////        existingPerson.setName(person.getName());
-////        existingPerson.setBirthDate(person.getBirthDate());
-////        existingPerson.setAdress(person.getAdress());
-//
-//        adressRepository.deleteAllById(listIds);
-//        personRepository.save(existingPerson);
-//
-//        return existingPerson;
-//    }
-
     public Person updatePerson(Long id, Person person) {
-//        Optional<Person> optionalPerson = Optional.ofNullable(personRepository.findById(id)
-//                .orElseThrow(() -> new IllegalStateException("Pessoa de ID" + id + "não foi encontrada.")));
-
-
-//        Person existingPerson ;
-//
-//        var listIds = optionalPerson.get().getAdress().stream().map(Adress::getId).collect(Collectors.toList());
-//
-//        existingPerson = new Person(person, id);
-//
-//
-//
-//        personRepository.save(existingPerson);
-//        adressRepository.deleteAllById(listIds);
-//
-//        return person;
 
         Person existingPerson = personRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("Pessoa de ID" + id + "não foi encontrada."));
