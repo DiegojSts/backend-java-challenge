6bf9de7fb395ef079cf20a4ef151bf87618aec81
Diego Santos <diegojs03@hotmail.com>
2023-01-17 09:08:13 -0300

    Renamed corrected 'adress' to 'address' in all project and created delete method

--- backend/demo/src/main/java/com/example/root/services/PersonService.java
+++ backend/demo/src/main/java/com/example/root/services/PersonService.java
@@ -1,28 +1,29 @@
 package com.example.root.services;
 import com.example.root.exception.NotFoundException;
-import com.example.root.model.Adress;
+import com.example.root.model.Address;
 import com.example.root.model.Person;
-import com.example.root.repository.AdressRepository;
+import com.example.root.repository.AddressRepository;
 import com.example.root.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private final PersonRepository personRepository;
-    private final AdressRepository adressRepository;
+    private final AddressRepository addressRepository;
 
-    public PersonService(PersonRepository personRepository, AdressRepository adressRepository) {
+    public PersonService(PersonRepository personRepository, AddressRepository addressRepository) {
         this.personRepository = personRepository;
-        this.adressRepository = adressRepository;
+        this.addressRepository = addressRepository;
     }
 
     public Person addPerson(Person person) throws IllegalArgumentException{
-        List<Adress> adresses = person.getAdress();
+        List<Address> addresses = person.getAddress();
 
-        long countTrue = adresses.stream().filter(Adress::isMainAdress).count();
+        long countTrue = addresses.stream().filter(Address::isMainAddress).count();
         if (countTrue > 1) throw new IllegalArgumentException("Deve existir apenas um endereço principal");
 
         return personRepository.save(person);
@@ -41,65 +42,76 @@ public class PersonService {
         return person.get();
     }
 
-    public List<Adress> findPersonAdressById(Long id) throws NotFoundException{
+    public List<Address> findPersonAddressById(Long id) throws NotFoundException{
         Optional<Person> person = Optional.ofNullable(personRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Endereço de ID" + id + "não foi encontrado.")));
+                .orElseThrow(() -> new NotFoundException("Busque por um novo ID, essa pessoa não está cadastrada")));
 
-        return person.get().getAdress();
+        return person.get().getAddress();
     }
 
-    public Person updatePerson(Long id, Person person) throws  NotFoundException{
+    public Person updatePerson(Long id, Person person) throws  NotFoundException, IllegalArgumentException{
+        List<Address> addresses = person.getAddress();
 
         Person existingPerson = personRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Pessoa de ID" + id + "não foi encontrada."));
+                .orElseThrow(() -> new NotFoundException("Pessoa de ID " + id + " não foi encontrada"));
+
+        long countTrue = addresses.stream().filter(Address::isMainAddress).count();
+        if (countTrue > 1) throw new IllegalArgumentException("Deve existir apenas um endereço principal");
+
 
         existingPerson.setName(person.getName());
         existingPerson.setBirthDate(person.getBirthDate());
 
-        List<Adress> newAdresses = person.getAdress();
-        List<Adress> currentAdresses = existingPerson.getAdress();
+        List<Address> newAddresses = person.getAddress();
+        List<Address> currentAddresses = existingPerson.getAddress();
 
-        for(Adress newAdress: newAdresses) {
+
+        for(Address newAddress: newAddresses) {
             boolean found = false;
-            for(Adress currentAdress: currentAdresses) {
-                if(newAdress.getId() == currentAdress.getId()) {
-                    currentAdress.setStreetAdress(newAdress.getStreetAdress());
-                    currentAdress.setState(newAdress.getState());
-                    currentAdress.setZipcode(newAdress.getZipcode());
-                    currentAdress.setCity(newAdress.getCity());
-                    currentAdress.setMainAdress(newAdress.isMainAdress());
+            for(Address currentAddress: currentAddresses) {
+                if(newAddress.getId() == currentAddress.getId()) {
+                    currentAddress.setStreetAddress(newAddress.getStreetAddress());
+                    currentAddress.setNumber(newAddress.getNumber());
+                    currentAddress.setZipcode(newAddress.getZipcode());
+                    currentAddress.setCity(newAddress.getCity());
+                    currentAddress.setMainAddress(newAddress.isMainAddress());
                     found = true;
                     break;
                 }
             }
             if(!found) {
-//                newAdress.setPerson(existingPerson);
-                currentAdresses.add(newAdress);
+                currentAddresses.add(newAddress);
             }
 
         }
 
-        List<Adress> adressesToRemove = new ArrayList<>();
-        for(Adress currentAdress: currentAdresses) {
+        List<Address> addressesToRemove = new ArrayList<>();
+        for(Address currentAddress: currentAddresses) {
             boolean found = false;
-            for(Adress newAdress: newAdresses) {
-                if(newAdress.getId() == currentAdress.getId()) {
+            for(Address newAddress: newAddresses) {
+                if(newAddress.getId() == currentAddress.getId()) {
                     found = true;
                     break;
                 }
             }
             if(!found) {
-                adressesToRemove.add(currentAdress);
+                addressesToRemove.add(currentAddress);
             }
         }
-        currentAdresses.removeAll(adressesToRemove);
-        existingPerson.setAdress(currentAdresses);
+        existingPerson.setAddress(currentAddresses);
 
         personRepository.save(existingPerson);
-        for (Adress adress: existingPerson.getAdress()) {
-            adressRepository.save(adress);
+        for (Address address: existingPerson.getAddress()) {
+            addressRepository.save(address);
         }
         return existingPerson;
+    }
+
+    public void deletePersonById(Long id) {
+        Person existingPerson = personRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Pessoa de ID " + id + " não foi encontrada"));
+
+        personRepository.deleteById(id);
     }
 }
 
